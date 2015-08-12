04fba248508354aac83a837ce0fd90d20435e2f7
yanghaibins <798075228@qq.com>
2015-08-12 21:08:21 +0800

    update

--- src/main/java/com/hand/Exam3_2/Exam3_2.java
+++ src/main/java/com/hand/Exam3_2/Exam3_2.java
@@ -14,7 +14,7 @@ public class Exam3_2 {
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila?characterEncoding=utf8", "root", "");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila?characterEncoding=utf8", "root", "798075228");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -28,39 +28,31 @@ public class Exam3_2 {
 		ResultSet rs=null;
 		ResultSet rs2=null;
 	try {//customer_id
-		String sql1="select film_id,title,last_update from "
-				+ "rental r,iventory i,film f where"
-				+ "r.customer_id="+id+" and r.iventory_id=i.iventory_id"
-				+ "and i.film_id=f.film_id";
-		
-		//String sql2="select f.film_id,f.title from film f where f.film_id=628";
-		
+		String sql1="select f.film_id,title,r.rental_date from "
+				+ "rental r,inventory i,film f where "
+				+ "r.customer_id="+id+" and r.inventory_id=i.inventory_id "
+				+ "and i.film_id=f.film_id order by r.rental_date desc " ;
+				
 		String sql3="select first_name,last_name from customer where customer_id="+id;
 		 st =conn.createStatement();
 		 
-		 rs= st.executeQuery(sql3); 
-		 
-		//rs= st.executeQuery(sql);
-		//int count=0;
-		//System.out.println("城市：");
+		rs= st.executeQuery(sql3); 
 		rs.first();
 		System.out.println(rs.getString("first_name")+"."+rs.getString("last_name")+"租用的film->");
 		System.out.println("film ID|film 名称	|租用时间");
 		
-		String sql2="select film_id,title from "
-				+ "film f where film_id =(selcet film_id from iventory i where "
-				+ "i.iventory_id =(select iventory_id from rental r where r.customer_id ="
-				+ "(select customer_id from custom c where c.customer_id="+id+"))";
+//		String sql2="select film_id,title from "
+//				+ "film f where film_id =(selcet film_id from iventory i where "
+//				+ "i.iventory_id =(select iventory_id from rental r where r.customer_id ="
+//				+ "(select customer_id from custom c where c.customer_id="+id+"))";
 		rs2= st.executeQuery(sql1); 
 		//rs.beforeFirst();
 		while (rs2.next()) {
-			//System.out.println("Country"+" "+rs.getString("country")+"的城市->");
-			//System.out.println("城市 ID|城市名称");
 			System.out.print(rs2.getString("f.film_id")+"  ");
 			System.out.print("|");
 			System.out.print(rs2.getString("f.title"));
 			System.out.print("|");
-			System.out.print(rs.getString("r2.last_update"));
+			System.out.print(rs2.getString("r.rental_date"));
 			System.out.println();
 		}
 		//System.out.println("done");
@@ -85,10 +77,9 @@ public class Exam3_2 {
 		}
 	}
 		
-	}
+}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("输入：");
 		System.out.print("请输入Customer Id:");
 		Scanner a1=new Scanner(System.in); 
@@ -97,8 +88,6 @@ public class Exam3_2 {
 		System.out.println("输出：");
 		check(customer_id);
 	}
-	
-	
-
+		
 }
 
